98b34cf1f556190fae1bd9fcacdd774f7600f0cf
om7057 <kulkarniom7057@gmail.com>
2024-10-05 16:32:10 +0530

    Footer Changes

--- techumen-website-frontend-demo/src/components/HomePage/Footer.jsx
+++ techumen-website-frontend-demo/src/components/HomePage/Footer.jsx
@@ -1,7 +1,5 @@
 import { useForm } from "react-hook-form";
 import logo from "../../images/acses.png";
-import { useState } from "react";
-
 
 function Footer() {
   const {
@@ -12,7 +10,7 @@ function Footer() {
   
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("/api/v1/contactUs/contact", {
+      const response = await fetch("https://techumen.onrender.com/api/v1/contactUs/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -24,14 +22,12 @@ function Footer() {
       if (response.ok) {
         alert("Message sent successfully!");
       } else {
-        // Attempt to parse the response as JSON
         let errorMessage;
         const contentType = response.headers.get("content-type");
         if (contentType && contentType.includes("application/json")) {
           const result = await response.json();
           errorMessage = result.error || "Failed to send the message.";
         } else {
-          // Fallback to a plain text response
           errorMessage = await response.text();
         }
         alert(`Failed to send the message: ${errorMessage}`);
@@ -41,7 +37,6 @@ function Footer() {
       alert("Error sending email. Please try again.");
     }
   };
-  
 
   return (
     <div className="footer-main-div z-10 text-gray-100 rounded-lg pt-8 relative mx-auto">
@@ -55,186 +50,44 @@ function Footer() {
           <p className="text-gray-200 text-md mb-4 nunito-font">
             Walchand College of Engineering, Sangli
           </p>
-          <div className="footer-social-link flex space-x-3">
-            <section className="flex justify-center items-center">
-              <button
-                href="/"
-                className="group flex justify-center p-2 rounded-md drop-shadow-xl bg-[#7289da] from-gray-800 to-black text-white font-semibold hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="white"
-                  stroke="white"
-                  width="1.5em"
-                  height="1.5em"
-                  viewBox="0 0 50 50"
-                >
-                  <path d="M 18.90625 7 C 18.90625 7 12.539063 7.4375 8.375 10.78125 C 8.355469 10.789063 8.332031 10.800781 8.3125 10.8125 C 7.589844 11.480469 7.046875 12.515625 6.375 14 C 5.703125 15.484375 4.992188 17.394531 4.34375 19.53125 C 3.050781 23.808594 2 29.058594 2 34 C 1.996094 34.175781 2.039063 34.347656 2.125 34.5 C 3.585938 37.066406 6.273438 38.617188 8.78125 39.59375 C 11.289063 40.570313 13.605469 40.960938 14.78125 41 C 15.113281 41.011719 15.429688 40.859375 15.625 40.59375 L 18.0625 37.21875 C 20.027344 37.683594 22.332031 38 25 38 C 27.667969 38 29.972656 37.683594 31.9375 37.21875 L 34.375 40.59375 C 34.570313 40.859375 34.886719 41.011719 35.21875 41 C 36.394531 40.960938 38.710938 40.570313 41.21875 39.59375 C 43.726563 38.617188 46.414063 37.066406 47.875 34.5 C 47.960938 34.347656 48.003906 34.175781 48 34 C 48 29.058594 46.949219 23.808594 45.65625 19.53125 C 45.007813 17.394531 44.296875 15.484375 43.625 14 C 42.953125 12.515625 42.410156 11.480469 41.6875 10.8125 C 41.667969 10.800781 41.644531 10.789063 41.625 10.78125 C 37.460938 7.4375 31.09375 7 31.09375 7 C 31.019531 6.992188 30.949219 6.992188 30.875 7 C 30.527344 7.046875 30.234375 7.273438 30.09375 7.59375 C 30.09375 7.59375 29.753906 8.339844 29.53125 9.40625 C 27.582031 9.09375 25.941406 9 25 9 C 24.058594 9 22.417969 9.09375 20.46875 9.40625 C 20.246094 8.339844 19.90625 7.59375 19.90625 7.59375 C 19.734375 7.203125 19.332031 6.964844 18.90625 7 Z M 18.28125 9.15625 C 18.355469 9.359375 18.40625 9.550781 18.46875 9.78125 C 16.214844 10.304688 13.746094 11.160156 11.4375 12.59375 C 11.074219 12.746094 10.835938 13.097656 10.824219 13.492188 C 10.816406 13.882813 11.039063 14.246094 11.390625 14.417969 C 11.746094 14.585938 12.167969 14.535156 12.46875 14.28125 C 17.101563 11.410156 22.996094 11 25 11 C 27.003906 11 32.898438 11.410156 37.53125 14.28125 C 37.832031 14.535156 38.253906 14.585938 38.609375 14.417969 C 38.960938 14.246094 39.183594 13.882813 39.175781 13.492188 C 39.164063 13.097656 38.925781 12.746094 38.5625 12.59375 C 36.253906 11.160156 33.785156 10.304688 31.53125 9.78125 C 31.59375 9.550781 31.644531 9.359375 31.71875 9.15625 C 32.859375 9.296875 37.292969 9.894531 40.3125 12.28125 C 40.507813 12.460938 41.1875 13.460938 41.8125 14.84375 C 42.4375 16.226563 43.09375 18.027344 43.71875 20.09375 C 44.9375 24.125 45.921875 29.097656 45.96875 33.65625 C 44.832031 35.496094 42.699219 36.863281 40.5 37.71875 C 38.5 38.496094 36.632813 38.84375 35.65625 38.9375 L 33.96875 36.65625 C 34.828125 36.378906 35.601563 36.078125 36.28125 35.78125 C 38.804688 34.671875 40.15625 33.5 40.15625 33.5 C 40.570313 33.128906 40.605469 32.492188 40.234375 32.078125 C 39.863281 31.664063 39.226563 31.628906 38.8125 32 C 38.8125 32 37.765625 32.957031 35.46875 33.96875 C 34.625 34.339844 33.601563 34.707031 32.4375 35.03125 C 32.167969 35 31.898438 35.078125 31.6875 35.25 C 29.824219 35.703125 27.609375 36 25 36 C 22.371094 36 20.152344 35.675781 18.28125 35.21875 C 18.070313 35.078125 17.8125 35.019531 17.5625 35.0625 C 16.394531 34.738281 15.378906 34.339844 14.53125 33.96875 C 12.234375 32.957031 11.1875 32 11.1875 32 C 10.960938 31.789063 10.648438 31.699219 10.34375 31.75 C 9.957031 31.808594 9.636719 32.085938 9.53125 32.464844 C 9.421875 32.839844 9.546875 33.246094 9.84375 33.5 C 9.84375 33.5 11.195313 34.671875 13.71875 35.78125 C 14.398438 36.078125 15.171875 36.378906 16.03125 36.65625 L 14.34375 38.9375 C 13.367188 38.84375 11.5 38.496094 9.5 37.71875 C 7.300781 36.863281 5.167969 35.496094 4.03125 33.65625 C 4.078125 29.097656 5.0625 24.125 6.28125 20.09375 C 6.90625 18.027344 7.5625 16.226563 8.1875 14.84375 C 8.8125 13.460938 9.492188 12.460938 9.6875 12.28125 C 12.707031 9.894531 17.140625 9.296875 18.28125 9.15625 Z M 18.5 21 C 15.949219 21 14 23.316406 14 26 C 14 28.683594 15.949219 31 18.5 31 C 21.050781 31 23 28.683594 23 26 C 23 23.316406 21.050781 21 18.5 21 Z M 31.5 21 C 28.949219 21 27 23.316406 27 26 C 27 28.683594 28.949219 31 31.5 31 C 34.050781 31 36 28.683594 36 26 C 36 23.316406 34.050781 21 31.5 21 Z M 18.5 23 C 19.816406 23 21 24.265625 21 26 C 21 27.734375 19.816406 29 18.5 29 C 17.183594 29 16 27.734375 16 26 C 16 24.265625 17.183594 23 18.5 23 Z M 31.5 23 C 32.816406 23 34 24.265625 34 26 C 34 27.734375 32.816406 29 31.5 29 C 30.183594 29 29 27.734375 29 26 C 29 24.265625 30.183594 23 31.5 23 Z"></path>
-                </svg>
+          
+          {/* Contact Form */}
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
+            <input
+              type="text"
+              placeholder="Your Name"
+              {...register("name", { required: "Name is required" })}
+              className={`border ${errors.name ? 'border-red-500' : 'border-gray-300'} p-2 mb-4 w-full`}
+            />
+            {errors.name && <p className="text-red-500">{errors.name.message}</p>}
 
-                <span className="pointer-events-none absolute opacity-0 group-hover:opacity-100 group-hover:text-white-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
-                  Discord
-                </span>
-              </button>
-            </section>
-            <section className="flex justify-center items-center">
-              <button
-                href="/"
-                className="group flex justify-center p-2 rounded-md drop-shadow-xl bg-[#0077b5] from-gray-800 to-black text-white font-semibold hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="1.5em"
-                  height="1.5em"
-                  color="#ffffff"
-                  fill="none"
-                >
-                  <path
-                    d="M4.5 9.5H4C3.05719 9.5 2.58579 9.5 2.29289 9.79289C2 10.0858 2 10.5572 2 11.5V20C2 20.9428 2 21.4142 2.29289 21.7071C2.58579 22 3.05719 22 4 22H4.5C5.44281 22 5.91421 22 6.20711 21.7071C6.5 21.4142 6.5 20.9428 6.5 20V11.5C6.5 10.5572 6.5 10.0858 6.20711 9.79289C5.91421 9.5 5.44281 9.5 4.5 9.5Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                  />
-                  <path
-                    d="M6.5 4.25C6.5 5.49264 5.49264 6.5 4.25 6.5C3.00736 6.5 2 5.49264 2 4.25C2 3.00736 3.00736 2 4.25 2C5.49264 2 6.5 3.00736 6.5 4.25Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                  />
-                  <path
-                    d="M12.326 9.5H11.5C10.5572 9.5 10.0858 9.5 9.79289 9.79289C9.5 10.0858 9.5 10.5572 9.5 11.5V20C9.5 20.9428 9.5 21.4142 9.79289 21.7071C10.0858 22 10.5572 22 11.5 22H12C12.9428 22 13.4142 22 13.7071 21.7071C14 21.4142 14 20.9428 14 20L14.0001 16.5001C14.0001 14.8433 14.5281 13.5001 16.0879 13.5001C16.8677 13.5001 17.5 14.1717 17.5 15.0001V19.5001C17.5 20.4429 17.5 20.9143 17.7929 21.2072C18.0857 21.5001 18.5572 21.5001 19.5 21.5001H19.9987C20.9413 21.5001 21.4126 21.5001 21.7055 21.2073C21.9984 20.9145 21.9985 20.4432 21.9987 19.5006L22.0001 14.0002C22.0001 11.515 19.6364 9.50024 17.2968 9.50024C15.9649 9.50024 14.7767 10.1531 14.0001 11.174C14 10.5439 14 10.2289 13.8632 9.995C13.7765 9.84686 13.6531 9.72353 13.505 9.63687C13.2711 9.5 12.9561 9.5 12.326 9.5Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+            <input
+              type="email"
+              placeholder="Your Email"
+              {...register("email", { required: "Email is required" })}
+              className={`border ${errors.email ? 'border-red-500' : 'border-gray-300'} p-2 mb-4 w-full`}
+            />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
 
-                <span className="pointer-events-none absolute opacity-0 group-hover:opacity-100 group-hover:text-white-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
-                  LinkedIn
-                </span>
-              </button>
-            </section>
-            <section>
-              <button
-                href="/"
-                className="group flex justify-center p-2 rounded-md drop-shadow-xl bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold hover:rounded-[50%] transition-all duration-500"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="1.5em"
-                  height="1.5em"
-                  color="#ffffff"
-                  fill="none"
-                >
-                  <path
-                    d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                  />
-                  <path
-                    d="M17.5078 6.5L17.4988 6.5"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+            <textarea
+              placeholder="Your Message"
+              {...register("message", { required: "Message is required" })}
+              className={`border ${errors.message ? 'border-red-500' : 'border-gray-300'} p-2 mb-4 w-full`}
+            />
+            {errors.message && <p className="text-red-500">{errors.message.message}</p>}
 
-                <span className="pointer-events-none absolute opacity-0 group-hover:opacity-100 group-hover:text-white-700 group-hover:text-sm group-hover:-translate-y-10 duration-700">
-                  Instagram
-                </span>
-              </button>
-            </section>
-          </div>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 transition"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
-
         
-        {/* Contact Form */}
-        <div className="footer-contact-div">
-      <h3 className="text-2xl text-white mb-3 tajawal-font">Contact Us</h3>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="space-y-3 tajawal-font"
-      >
-        <div>
-        <label
-            htmlFor="name"
-            className="block text-white-700 text-md font-bold mb-2"
-          >
-          Name:
-        </label>
-        <input
-            id="name"
-            className="text-black custom-input w-full px-4 py-2 border border-gray-600 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
-            type="text"
-            disabled={false}
-            placeholder="Enter your Name"
-            {...register("name", { required: "Name is required" })}
-          />
-          <label
-            htmlFor="email"
-            className="block text-white-700 text-md font-bold mb-2"
-          >
-            Email:
-          </label>
-          <input
-            id="email"
-            className="text-black custom-input w-full px-4 py-2 border border-gray-600 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
-            type="text"
-            disabled={false}
-            placeholder="Enter your Email"
-            {...register("email", { required: "Email is required" })}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.email.message}
-            </p>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="message"
-            className="block text-white-700 text-md font-bold mb-2"
-          >
-            Message:
-          </label>
-          <textarea
-            className="text-black custom-input w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
-            {...register("message", {
-              required: "Message is required",
-              minLength: {
-                value: 10,
-                message: "Message should be at least 10 characters long",
-              },
-            })}
-            placeholder="Enter your message"
-            rows="3"
-          ></textarea>
-          {errors.message && (
-            <p className="text-black text-xs mt-1">
-              {errors.message.message}
-            </p>
-          )}
-        </div>
-        <button className="cursor-pointer font-semibold overflow-hidden relative z-100 border border-blue-500 group px-8 py-2">
-          <span className="relative z-10 text-blue-500 group-hover:text-white text-md duration-500">
-            Submit
-          </span>
-          <span className="absolute w-full h-full bg-blue-500 -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
-          <span className="absolute w-full h-full bg-blue-500 -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
-        </button>
-      </form>
-    </div>
+        {/* Other Footer Sections */}
+        {/* ... Your existing footer sections ... */}
       </div>
-      <p className="text-center text-white text-lg left-[43%] absolute bottom-2 z-50 mt-6 fredoka-font">
-        © ACSES 2024. All Rights Reserved
-      </p>
     </div>
   );
 }
